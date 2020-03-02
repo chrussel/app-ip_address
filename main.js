405280c0ef77294e92138ee53442c77e2b925249
chrussel <christine.russell@centurylink.com>
2020-03-02 15:00:24 +0000

    Lab 3

--- main.js
+++ main.js
@@ -128,20 +128,6 @@ function main() {
       console.log(`  Response returned from GET request: ${data}`);
     });
   }
-  // Iterate over sampleIpv4s and pass the element's value to getIpv4MappedIpv6Address().
-  for (let i = 0; i < sampleIpv4sLen; i++) {
-    console.log(`\n--- Test Number ${i + 1} getIpv4MappedIpv6Address(${sampleIpv4s[i]}) ---`);
-    // Assign the function results to a variable so we can check if a string or null was returned.
-    let mappedAddress = getIpv4MappedIpv6Address(sampleIpv4s[i]);
-    if( mappedAddress ) {
-      console.log(`  IPv4 ${sampleIpv4s[i]} mapped to IPv6 Address: ${mappedAddress}`);
-    } else {
-      console.error(`  Problem converting IPv4 ${sampleIpv4s[i]} into a mapped IPv6 address.`);
-    }
-  }
-}
-
-/*
-  Call main to run it.
+    Call main to run it.
 */
 main();
